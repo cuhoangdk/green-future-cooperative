6109d66c8fccf918c3b4b782cdea47bc41f08537
cuhoangdk <104707960+cuhoangdk@users.noreply.github.com>
2025-01-20 17:19:42 +0700

    quên bắt validate :V

--- backend/app/Http/Requests/StoreUpdateCategoryRequest.php
+++ backend/app/Http/Requests/StoreUpdateCategoryRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreUpdateCategoryRequest extends FormRequest
 {
@@ -25,5 +27,14 @@ class StoreUpdateCategoryRequest extends FormRequest
             'name' => 'required|string|max:255',
             'description' => 'nullable|string|max:500',
         ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422)
+        );
     }
 }
