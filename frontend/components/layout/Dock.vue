dbafe88dbfe0c6bdfdf8c4a828baad686516e58e
Kassa <nhokkho18@gmail.com>
2025-04-11 10:56:06 +0700

    feat: thêm bottom bar

---
+++ frontend/components/layout/Dock.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="lg:hidden dock dock-xs">
+        <NuxtLink to="/orders" :class="{ 'dock-active': route.path === '/orders' }">
+            <ShoppingBag class="size-[1.2em]" color="currentColor" />
+        </NuxtLink>
+
+        <NuxtLink to="/account/addresses" :class="{ 'dock-active': route.path === '/account/addresses' }">
+            <MapPin class="size-[1.2em]" color="currentColor" />
+        </NuxtLink>
+
+        <NuxtLink to="/account" :class="{ 'dock-active': route.path === '/account' }">
+            <User class="size-[1.2em]" color="currentColor" />
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { User, MapPin, ShoppingBag } from 'lucide-vue-next';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
