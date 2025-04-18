9bb27888bce76800cef6559979dfddf505f672ed
Kassa <nhokkho18@gmail.com>
2025-04-18 13:07:00 +0700

    fix: tăng chiều cao carousel

--- frontend/components/ui/Carousel.vue
+++ frontend/components/ui/Carousel.vue
@@ -1,9 +1,9 @@
 <!-- components/Carousel.vue -->
 <template>
-    <div class="carousel w-full h-80 bg-gray-100">
+    <div class="carousel w-full h-96 bg-gray-100">
         <div v-for="(slide, index) in slides" :key="slide.id" :id="`slide${index + 1}`"
             class="carousel-item relative w-full">
-            <img :src="backendUrl + slide.image_url" :alt="slide.title" class="w-full h-80 object-cover" />
+            <img :src="backendUrl + slide.image_url" :alt="slide.title" class="w-full h-96 object-cover" />
             <div class="absolute left-1 right-1 top-1/2 flex -translate-y-1/2 transform justify-between">
                 <a :href="`#slide${index === 0 ? slides.length : index}`" class="btn btn-circle opacity-30">❮</a>
                 <a :href="`#slide${index === slides.length - 1 ? 1 : index + 2}`" class="btn btn-circle opacity-30">❯</a>
